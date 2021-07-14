22e46c5d67bfd2e1ad8dae21816289200c8c296c
ojea <52174468+Kwonojae@users.noreply.github.com>
2021-07-15 00:19:25 +0900

    add habit tracker

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import "./index.css";
 // import App from "./app";
 import "@fortawesome/fontawesome-free/js/all.js";
 import SimpleHabit from "./components/simpleHabit";
+import App from "./app";
 
 ReactDOM.render(
   <React.StrictMode>
-    <SimpleHabit />
+    <App />
   </React.StrictMode>,
   document.getElementById("root") //root 라는 아이디를 가진 요소를 제일 상위에 있는 app컴포넌트를 연결시켜주는 부분
 );
